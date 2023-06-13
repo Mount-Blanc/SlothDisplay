4dbc808c17f9c287f5907aa41df71f0ca6cc228d
Mount-Blanc <mountblanc100@gmail.com>
2023-06-13 12:39:09 -0700

    Initialized project

---
+++ SlothDisplay/src/main/java/Sloth/SlothDisplay/SlothDisplayApplication.java
@@ -0,0 +1,13 @@
+package Sloth.SlothDisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlothDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlothDisplayApplication.class, args);
+	}
+
+}
---
+++ SlothDisplay/src/test/java/Sloth/SlothDisplay/SlothDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package Sloth.SlothDisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlothDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
