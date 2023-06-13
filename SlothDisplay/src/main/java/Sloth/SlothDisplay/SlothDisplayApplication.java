d802e93f61e5109f26b2e807e179fd0080fd52b7
Mount-Blanc <mountblanc100@gmail.com>
2023-06-13 12:41:10 -0700

    Initialized project

--- SlothDisplay/src/main/java/Sloth/SlothDisplay/SlothDisplayApplication.java
+++ SlothDisplay/src/main/java/Sloth/SlothDisplay/SlothDisplayApplication.java
@@ -3,6 +3,13 @@ package Sloth.SlothDisplay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SlothDisplayApplication {
 
